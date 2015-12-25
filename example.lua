87a4c00bf1482939c77c60569363547a65d2d224
Brendan Shillingford <brendan.shillingford@cs.ox.ac.uk>
2015-12-25 13:04:30 +0000

    Add better example

--- example.lua
+++ example.lua
@@ -1,16 +1,39 @@
 package.path = package.path .. ';./?/init.lua'
 
 require 'nn'
+require 'nngraph'
 nnq = require 'nnquery'
-
-ctx = nnq.Context()
-ctx:reg(nnq.ContainerElement, nnq.ContainerElement.isContainer) -- nn.Container s
---ctx:default(nnq.ChildlessElement) -- root nodes
-ctx:reg(nnq.ChildlessElement, function(x) return not nnq.ContainerElement.isContainer(x) end)
 
 seq = nn.Sequential()
   :add(nn.Linear(3,4))
   :add(nn.Container():add(nn.ReLU()):add(nn.Tanh()))
 
-e = ctx(seq)
+function new_LSTM(opt)
+    local x = nn.Identity()()
+    local prev_c = nn.Identity()()
+    local prev_h = nn.Identity()()
+
+    function new_input_sum()
+        -- transforms input
+        local i2h            = nn.Linear(opt.rnn_size, opt.rnn_size)(x)
+        -- transforms previous timestep's output
+        local h2h            = nn.Linear(opt.rnn_size, opt.rnn_size)(prev_h)
+        return nn.CAddTable()({i2h, h2h})
+    end
+
+    local in_gate          = nn.Sigmoid()(new_input_sum())
+    local forget_gate      = nn.Sigmoid()(new_input_sum())
+    local out_gate         = nn.Sigmoid()(new_input_sum())
+    local in_transform     = nn.Tanh()(new_input_sum())
+
+    local next_c           = nn.CAddTable()({
+        nn.CMulTable()({forget_gate, prev_c}),
+        nn.CMulTable()({in_gate,     in_transform})
+    })
+    local next_h           = nn.CMulTable()({out_gate, nn.Tanh()(next_c)})
+
+    nngraph.annotateNodes()
+    return nn.gModule({x, prev_c, prev_h}, {next_c, next_h})
+end
+lstm = new_LSTM{rnn_size=3}
 
